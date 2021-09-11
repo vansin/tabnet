95edee24fddc67904bc1ce50f9632292bc5b11a2
vansin <vansin@aliyun.com>
2021-09-11 21:34:08 +0800

    feat: small datasets

--- tools/split_datasets.py
+++ tools/split_datasets.py
@@ -1,1 +1,28 @@
 import json
+
+# Opening JSON file
+f = open('/home/tml/vansin/paper/tabnet/data/table/annotations/tablebank_word_train.json', )
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+
+
+images_ori = data['images']
+# data = {}
+data.pop('images')
+
+images = []
+for i,image in enumerate(images_ori):
+    if i % 100 == 0:
+        images.append(image)
+
+data['images'] = images
+
+
+with open("/home/tml/vansin/paper/tabnet/data/table/annotations/tablebank_word_train_son.json", "w") as outfile:
+    json.dump(data, outfile)
+
+
+print(data)
