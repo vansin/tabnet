0581ffcb028b89d27f47ca6fb1c1772cd61d4e9b
vansin <vansin@aliyun.com>
2021-12-28 15:25:16 +0800

    feat: add prepare_datasets_local

---
+++ tools/prepare_datasets_local.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mkdir /tmp/vansin_ram
+
+rm -rf data
+rm -rf work_dirs
+
+sudo mount -t tmpfs -o size=72G tmpfs /tmp/vansin_ram
+
+cp /media/tml/data_vansin/datasets.zip /tmp/vansin_ram/datasets.zip
+unzip /tmp/vansin_ram/datasets.zip -d /tmp/vansin_ram
+ln -s /tmp/vansin_ram/datasets $(pwd)/data
+rm -rf /tmp/vansin_ram/datasets.zip
+
+ln -s /media/tml/data_tml/weight_files/tabnet/work_dirs $(pwd)/work_dirs
