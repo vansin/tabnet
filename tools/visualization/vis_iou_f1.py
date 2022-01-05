ad4bd861b51fe2716ee68070fe477b26306aac51
vansin <vansin@aliyun.com>
2022-01-05 17:27:55 +0800

    feat: vis_iou_f1

---
+++ tools/visualization/vis_iou_f1.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+import matplotlib.pyplot as plt
+import mmcv
+import pandas as pd
+import seaborn as sns
+
+if __name__ == '__main__':
+
+    work_dirs = os.listdir('work_dirs')
+
+    results = []
+
+    for i, work_dir in enumerate(work_dirs):
+        work_dir_files = os.listdir('work_dirs/' + work_dir)
+        eval_files = []
+        config_file = None
+        for file_name in work_dir_files:
+            if file_name.endswith('_eval.json'):
+
+                name = 'work_dirs/' + work_dir + '/' + file_name
+                data_origin = mmcv.load(name)
+
+                epoch = int(name.split('/')[-1].split('_')[1])
+
+                data = dict()
+                data['epoch'] = epoch
+                config_name = data_origin['config'].split('/')[-1]
+                data['config'] = config_name
+                data.update(data_origin['metric'])
+                iou_curves = data_origin['metric']['iou_ap_recll_f2_list']
+                df = pd.DataFrame.from_dict(iou_curves)
+                g = sns.lineplot(x='iou', y='f1_score', data=df, markers=True, dashes=False)
+                # g.legend(loc='right', bbox_to_anchor=(1.5, 0.5), ncol=1)
+                plt.show()
+                print(plt)
+
+                eval_files.append(data)
+            if file_name.endswith('.py'):
+                config_file = 'work_dirs/' + work_dir + '/' + file_name
+
+        eval_files.sort(key=lambda x: x['epoch'])
+        results.append(eval_files)
+
+    print(results)
+
+
+intput_data = []
+
+
+for result in results:
+
+    intput_data.extend(result)
+
+    pass
+
+with open('/home/tml/Nutstore Files/ubuntu/paper/data/1.json', 'w') as f:
+    json.dump(results, f)
+
+df = pd.DataFrame.from_dict(intput_data)
+
+
+df.to_csv('/home/tml/Nutstore Files/ubuntu/paper/data/1.csv')
+
+
+g = sns.lineplot(x='iou', y='f1_score', data=df, hue='config',
+                 style='config', markers=True, dashes=False)
+# g.legend(loc='right', bbox_to_anchor=(1.5, 0.5), ncol=1)
+
+plt.show()
+print(plt)
+# for result in results:
+#
+#     sns.set_theme(style='darkgrid')
+#     # Load an example dataset with long-form data
+#     df = pd.DataFrame.from_dict(result)
+#
+#     # Plot the responses for different events and regions
+#     sns.lineplot(x='epoch', y='bbox_mAP',
+#                  data=df)
+#
+#     plt.show()
